9ced518483fd74ab9278a1b4b89a12d6fe171c94
seleniumproject <karanamseshubabu27@gmail.com>
2023-06-03 14:58:14 +0530

    myfirstcommit

---
+++ src/test/java/pageobjects/HomePage.java
@@ -0,0 +1,38 @@
+package pageobjects;
+
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.WebElement;
+	import org.openqa.selenium.support.FindBy;
+
+	public class HomePage extends BasePage {
+
+		public HomePage(WebDriver driver) {
+			super(driver);
+		}
+
+		// Elements
+	
+
+		
+		@FindBy(xpath=("//input[@name='txtuId']"))
+		WebElement email;
+		@FindBy(xpath=("//input[@name='txtPword']"))
+		WebElement password;
+        @FindBy(xpath=("//input[@name='login']"))
+        WebElement login;
+		// Action Methods
+		
+		public void EnterEmail(String name) {
+			email.sendKeys(name);
+		}
+		public void EnterPassword(String name) {
+			password.sendKeys(name);
+		}
+		public void ClickOnLogin() {
+			login.click();
+		}
+		
+		
+	}
+
+
---
+++ src/test/java/testcases/TC_001_Login.java
@@ -0,0 +1,34 @@
+package testcases;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.testng.annotations.Test;
+
+import pageobjects.HomePage;
+import testbase.BaseClass;
+
+public class TC_001_Login extends BaseClass {
+
+	FileInputStream file;
+	Properties pro;
+
+	@Test
+	public void login() throws IOException {
+		logger.info("***TC_001_Login ******");
+		file = new FileInputStream("D:\\seleniumproject\\src\\test\\resources\\dynamic.properties");
+		pro = new Properties();
+		pro.load(file);
+
+		HomePage homepage = new HomePage(driver);
+		logger.info("***created homepageobject ******");
+		homepage.EnterEmail(pro.getProperty("email"));
+		logger.info("***EnterEmailAddress ******");
+		homepage.EnterPassword(pro.getProperty("password"));
+		logger.info("***EnterPassword ***");
+		homepage.ClickOnLogin();
+		logger.info("***ClickedOnLoginButton ***");
+
+	}
+}
